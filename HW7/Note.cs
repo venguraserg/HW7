24fa6fe11443dcb4b5e726459bd0338e13281830
Венгура Сергей <venguraserg@mail.ru>
2021-08-18 23:21:30 +0300

    Добавьте файлы проекта.

---
+++ HW7/Note.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7
+{
+    public class Note
+    {
+        //свойства
+        public int Id { get; }
+        public DateTime CreateTime { get; }
+        public string Content { get; set; }
+        public string Creator { get; set; }
+        public Status Status { get; set; }
+        private static int _count = 0;
+        //конструктор
+
+        public Note(string content, string creator, Status status)
+        {
+            Id = _count++;
+            CreateTime = DateTime.Now;
+            Content = content;
+            Creator = creator;
+            Status = status;
+        }
+        public override string ToString()
+        {
+            return $"{Id}  {CreateTime}  {Content}  {Creator}   {Status}";
+        }
+    }
+}
---
+++ HW7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Note> notebook = new List<Note>();
+            notebook = Autocomplete(100);
+
+            Noteboock book = new Noteboock();
+
+            book.Records = Autocomplete(100);
+
+            foreach (var item in book.Records)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine("Конец программы. Для продолжения нажмите любую клавишу . . . ");
+            Console.ReadKey();
+        }
+
+        private static List<Note> Autocomplete(int items)
+        {
+            List<Note> notebook = new List<Note>();
+            for (int i = 0; i < items; i++)
+            {
+                notebook.Add(new Note("content" + i, "creator" + i, Status.Actual));
+            }
+            return notebook;
+        }
+    }
+}
